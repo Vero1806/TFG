fb052b2e96e5834c511e7d7afe3b6a9bdc7a520d
Vprieto <146030980+Vero1806@users.noreply.github.com>
2024-05-24 20:59:10 +0200

    Pruebas conexiones sin abstracción

--- app/src/main/java/com/example/tfg/BBDD/Conexion.kt
+++ app/src/main/java/com/example/tfg/BBDD/Conexion.kt
@@ -4,11 +4,11 @@ import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
-abstract class Conexion {
+class Conexion {
 
     //Cuando se usa ? detrás de una sentencia de conexión se está haciendo una especie de TERNARIO lo que permite controlar
     //las excepciones de respuestas nulas por parte de la base de datos. En este Package de base de datos lo veremos mucho.
-    abstract var conexion: Connection?
+    var conexion: Connection? = null
 
     //abre la conexión con la base de datos y devuelve null si falla
     fun establecerConexion() {
